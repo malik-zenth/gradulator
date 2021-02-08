68658b603f4e064dd12f5db1f66a409b8f36ef9e
Majeed Malik <mmalik1@stud.hs-heilbronn.de>
2021-02-08 11:34:32 +0100

    Added new text

--- src/Components/ManualDataEntry.tsx
+++ src/Components/ManualDataEntry.tsx
@@ -7,7 +7,7 @@ class ManualDataEntry extends React.Component {
         return (
             <div>
                 <Card title="Manuelle Noteneingabe" bordered={false} bodyStyle={{paddingBottom: 0}}>
-                    <p>Lasse deinen Notendurchschnitt berechnen, indem du manuell deine aktuellen Noten eingibst</p>
+                    <p>Lasse deinen Notendurchschnitt berechnen, indem du manuell deine aktuellen Noten eingibst. Hinweis: Deinen aktuellen Notenspiegel findest du unter https://stud.zv.hs-heilbronn.de/</p>
                 </Card>
             </div>
         )
