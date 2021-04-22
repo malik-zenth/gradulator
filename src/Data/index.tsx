da8c3a18a6c0ab32ba04d159c794c3c59b8b4d59
Majeed Malik <mmalik1@stud.hs-heilbronn.de>
2021-04-22 17:53:05 +0200

    Added BU

--- src/Data/index.tsx
+++ src/Data/index.tsx
@@ -3,10 +3,12 @@ import {win} from "./win"
 import {mid} from "./mid"
 import {mwi} from "./mwi"
 import { vb } from "./vb";
+import { bu } from "./bu";
 
 export const options: DegreeOptions = {
     //"Verkehrsbetriebswirtschaft und Logistik": vb,
     "Wirtschaftsinformatik": win,
     "Wirtschaftsinformatik - Informationsmanagement und Data Science": mid,
-    "Wirtschaftsinformatik - Digitale Transformation": mwi
+    "Wirtschaftsinformatik - Digitale Transformation": mwi,
+    "Betriebswirtschaft und Unternehmensführung": bu
 }
